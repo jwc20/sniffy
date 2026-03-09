8f4f044028edd9e91f52216a5e353c88c26aa9be
jw <choi.jae100@gmail.com>
2026-03-09 15:33:29 -0700

    add simple tui

--- cmd/tui/main.go
+++ cmd/tui/main.go
@@ -1,1 +1,96 @@
 package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
+)
+
+type model struct {
+	altscreen bool
+	width     int
+	height    int
+}
+
+func main() {
+	m := model{
+		altscreen: true,
+	}
+
+	// Log to a file. Useful in debugging since you can't really log to stdout.
+	// Not required.
+	logfilePath := os.Getenv("BUBBLETEA_LOG")
+	if logfilePath != "" {
+		if _, err := tea.LogToFile(logfilePath, "simple"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+		return m, nil
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "ctrl+z":
+			return m, tea.Suspend
+		}
+	}
+	return m, nil
+}
+
+func (m model) center(content string) string {
+	if m.width == 0 || m.height == 0 {
+		return content
+	}
+	lines := strings.Split(strings.TrimRight(content, "\n"), "\n")
+	maxWidth := 0
+	for _, line := range lines {
+		if w := lipgloss.Width(line); w > maxWidth {
+			maxWidth = w
+		}
+	}
+	leftPad := (m.width - maxWidth) / 2
+	if leftPad < 0 {
+		leftPad = 0
+	}
+	prefix := strings.Repeat(" ", leftPad)
+	topPad := (m.height - len(lines)) / 2
+	if topPad < 0 {
+		topPad = 0
+	}
+	var sb strings.Builder
+	sb.WriteString(strings.Repeat("\n", topPad))
+	for _, line := range lines {
+		sb.WriteString(prefix + line + "\n")
+	}
+	return sb.String()
+}
+
+func (m model) View() tea.View {
+	content := "Hello word"
+	if m.altscreen {
+		content = m.center(content)
+	}
+
+	v := tea.NewView(content)
+	v.AltScreen = m.altscreen
+	return v
+}
--- go.mod
+++ go.mod
@@ -2,18 +2,31 @@ module github.com/jwc20/sniffa
 
 go 1.25.1
 
+require github.com/fsnotify/fsnotify v1.9.0
+
 require (
-	github.com/fsnotify/fsnotify v1.9.0 // indirect
-	gotest.tools/gotestsum v1.13.0
+	charm.land/bubbletea/v2 v2.0.2
+	github.com/fatih/color v1.18.0
+	golang.org/x/sys v0.42.0
 )
 
 require (
-	github.com/bitfield/gotestdox v0.2.2 // indirect
-	github.com/fatih/color v1.18.0 // indirect
+	charm.land/lipgloss/v2 v2.0.1 // indirect
+	github.com/charmbracelet/colorprofile v0.4.2 // indirect
+	github.com/charmbracelet/ultraviolet v0.0.0-20260205113103-524a6607adb8 // indirect
+	github.com/charmbracelet/x/ansi v0.11.6 // indirect
+	github.com/charmbracelet/x/term v0.2.2 // indirect
+	github.com/charmbracelet/x/termios v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.2.2 // indirect
+	github.com/clipperhouse/displaywidth v0.11.0 // indirect
+	github.com/clipperhouse/stringish v0.1.1 // indirect
+	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sync v0.17.0 // indirect
-	golang.org/x/sys v0.36.0 // indirect
-	golang.org/x/term v0.35.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	github.com/mattn/go-runewidth v0.0.19 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sync v0.19.0 // indirect
 )
